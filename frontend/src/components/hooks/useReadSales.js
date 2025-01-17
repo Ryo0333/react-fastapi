f1ec6af2eab0e744b8c64613a8885df20c9184e6
Ryo0333 <windsguitar@icloud.com>
2025-01-17 18:25:14 +0900

    trailing slashの変更に伴う修正

--- frontend/src/components/hooks/useReadSales.js
+++ frontend/src/components/hooks/useReadSales.js
@@ -4,7 +4,7 @@ export const useReadSales = () => {
   const baseEndpoint = "http://127.0.0.1:8000/sales";
 
   const onClickReadSales = async (year) => {
-    const endpoint = baseEndpoint + year;
+    const endpoint = baseEndpoint + "/" + year;
     const res = await axios.get(endpoint);
     return res.data;
   };
