1a68c16b21a3e4ab00bc7fab1b241cfdc24ffdac
Ryo0333 <windsguitar@icloud.com>
2025-01-30 20:54:02 +0900

    import文の修正

--- backend/api/endpoints/users.py
+++ backend/api/endpoints/users.py
@@ -4,13 +4,13 @@ from sqlalchemy.orm import Session
 from api.crud import users
 from api.endpoints.deps import get_db
 from core.security import verify_password
-from schemas import user
+from schemas.user import UserCreate, UserInDB
 
 user_router = APIRouter()
 
 
-@user_router.post("/users", response_model=user.User)
-def create_user(user: user.UserCreate, db: Session = Depends(get_db)):
+@user_router.post("/users", response_model=UserInDB)
+def create_user(user: UserCreate, db: Session = Depends(get_db)):
     db_user = users.get_user_by_name(db, name=user.name)
     if db_user:
         raise HTTPException(status_code=400, detail="Name already registered")
@@ -18,7 +18,7 @@ def create_user(user: user.UserCreate, db: Session = Depends(get_db)):
     return users.create_user(db=db, user=user)
 
 
-@user_router.get("/users", response_model=user.User)
+@user_router.get("/users", response_model=UserInDB)
 def read_user(name: str, password: str, db: Session = Depends(get_db)):
     db_user = users.get_user_by_name(db=db, name=name)
     if not db_user:
